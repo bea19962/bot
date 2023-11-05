81fac949169f06ca8987c68e9b34d80d73e10775
BananaGrill <bea_19962@hotmail.com>
2023-11-05 19:18:44 +0000

    FINALLY IT WORKS

--- commands/fun/gif.js
+++ commands/fun/gif.js
@@ -1,9 +1,8 @@
 const axios = require('axios');
 const { SlashCommandBuilder } = require('discord.js');
 
-//const GIPHY_API_URL = 'https://api.giphy.com/v1/gifs/search';
-const GIPHY_API_URL = 'https://api.giphy.com/v1/gifs/random?api_key=t333mUFtGiFelxW8NBTwlW6VxCLDoR9e&tag=burrito&rating=pg';
-//onst GIPHY_API_KEY = 't333mUFtGiFelxW8NBTwlW6VxCLDoR9e';
+const GIPHY_API_URL = 'https://api.giphy.com/v1/gifs/random';
+const GIPHY_API_KEY = 't333mUFtGiFelxW8NBTwlW6VxCLDoR9e';
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -13,35 +12,19 @@ module.exports = {
   async execute(interaction) {
     try {
       const response = await axios.get(GIPHY_API_URL, {
-        // params: {
-        //   api_key: GIPHY_API_KEY,
-        //   tag: 'cool',
-        //   rating: 'pg',
-        // },
+        params: {
+          api_key: GIPHY_API_KEY,
+          tag: 'cool',
+          rating: 'pg',
+        },
       });
-
-      console.log('response', response.data.data);
-      console.log('res', response);
 
       if (response.data.data.length === 0) {
         throw new Error('Giphy returned no results.');
       }
 
-      // const gifUrl = response.data.data.images.original.url;
-      const gifUrl = response.data;
-      console.log('gif', response.data.data.images.original.url);
-
-      await interaction.reply( gifUrl
-      //  {
-      //   embeds: [
-      //     {
-      //       image: {
-      //         url: gifUrl,
-      //       },
-      //     },
-      //   ],
-      // }
-      );
+      const gifUrl = response.data.data.images.original.url;
+      return interaction.reply(gifUrl);
     } catch (error) {
       console.error('Error fetching GIF from GIPHY:', error);
       await interaction.reply({
