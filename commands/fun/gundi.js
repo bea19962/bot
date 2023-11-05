6b7cc1b7ca8209a8454cdaca41b674ccdf591353
BananaGrill <bea_19962@hotmail.com>
2023-11-05 19:26:13 +0000

    linter

--- commands/fun/gundi.js
+++ commands/fun/gundi.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('gundi')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		return interaction.reply('Gundolf!');
-	},
+  data: new SlashCommandBuilder()
+    .setName('gundi')
+    .setDescription('Replies with Pong!'),
+  async execute(interaction) {
+    return interaction.reply('Gundolf!');
+  },
 };
