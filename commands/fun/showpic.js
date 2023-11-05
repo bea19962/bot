2cebfff67b9015e6ee5aca4a863de0b1ae444099
BananaGrill <bea_19962@hotmail.com>
2023-11-05 20:57:15 +0000

    new command

---
+++ commands/fun/showpic.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('showpic')
+    .setDescription("Retrieves the user's image")
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('The user to show the avatar of')
+        .setRequired(false),
+    ),
+  async execute(interaction) {
+    const user = interaction.options.getUser('user') || interaction.user;
+    const avatarURL = user.displayAvatarURL({ dynamic: true, size: 1024 });
+
+    const embed = new EmbedBuilder()
+      .setTitle(`${user.username}'s Avatar`)
+      .setImage(avatarURL);
+
+    await interaction.reply({ embeds: [embed] });
+  },
+};
