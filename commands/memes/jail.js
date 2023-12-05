e8223e54c72d3509fb7e47ef25a17a32091dccb9
BananaGrill <bea_19962@hotmail.com>
2023-12-05 22:27:13 +0000

    lint

--- commands/memes/jail.js
+++ commands/memes/jail.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const Jimp = require('jimp');
+const sharp = require('sharp');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,23 +16,35 @@ module.exports = {
   async execute(interaction) {
     const user = interaction.options.getUser('user') || interaction.user;
     const avatarURL = user.displayAvatarURL({ format: 'png', size: 1024 });
-    let overlay = await Jimp.read('src/templates/jail_bars.png');
+    const overlay = await Jimp.read('src/templates/jail_bars.png');
+
     overlay = overlay.resize(1024, 1024);
+
     const avatar = await Jimp.read(avatarURL);
-
-    avatar.composite(overlay, 0, 0, {
-      mode: Jimp.BLEND_SOURCE_OVER,
-      opacityDest: 1,
-      opacitySource: 0.5,
-    });
-
-    const buffer = await avatar.getBufferAsync(Jimp.MIME_PNG);
-    const attachment = { files: [{ attachment: buffer, name: 'jail.png' }] };
-
-    const embed = new EmbedBuilder()
-      .setTitle(`${user.username} is in jail!`)
-      .setImage('attachment://jail.png');
-
-    await interaction.reply({ embeds: [embed], files: [attachment] });
-  },
-};
+    avatar.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
+     if (err) throw new Error(err);
+   
+     // Convert WebP to PNG using sharp
+     sharp(buffer)
+       .toFormat('png')
+       .toBuffer()
+       .then(async (convertedBuffer) => {
+         const avatarImage = await Jimp.read(convertedBuffer);
+         avatarImage.composite(overlay, 0, 0, {
+           mode: Jimp.BLEND_SOURCE_OVER,
+           opacityDest: 1,
+           opacitySource: 0.5,
+         });
+   
+         const finalBuffer = await avatarImage.getBufferAsync(Jimp.MIME_PNG);
+         const attachment = { files: [{ attachment: finalBuffer, name: 'jail.png' }] };
+   
+         const embed = new EmbedBuilder()
+           .setTitle(`${user.username} is in jail!`)
+           .setImage('attachment://jail.png');
+   
+         await interaction.reply({ embeds: [embed], files: [attachment] });
+       })
+  }),
+}
+}
