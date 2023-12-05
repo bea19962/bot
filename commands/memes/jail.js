7632cc6790214161e709a705dfdc66a6ca93527e
BananaGrill <bea_19962@hotmail.com>
2023-12-05 23:19:46 +0000

    aaaa

--- commands/memes/jail.js
+++ commands/memes/jail.js
@@ -1,50 +1,44 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const Jimp = require('jimp');
-const sharp = require('sharp');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('jail')
-    .setDescription('Send somebody to jail')
-    .addUserOption(
-      option =>
-        option
-          .setName('user')
-          .setDescription('The user to show the avatar of')
-          .setRequired(false), // Optional to choose a user
+    .setDescription('Send the user to jail')
+    .addUserOption(option =>
+      option
+        .setName('user')
+        .setDescription('The user to send to jail') // always add description because of the deployment
+        .setRequired(false),
     ),
   async execute(interaction) {
+    let overlay = await Jimp.read('src/templates/jail_bars.png');
+    overlay.resize(1024, 1024);
+
+    //avatar
     const user = interaction.options.getUser('user') || interaction.user;
-    const avatarURL = user.displayAvatarURL({ format: 'png', size: 1024 });
-    const overlay = await Jimp.read('src/templates/jail_bars.png');
+    const avatarURL = user.displayAvatarURL({ extension: 'png', size: 1024 });
 
-    overlay = overlay.resize(1024, 1024);
+    const avatarJimp = await Jimp.read(avatarURL);
 
-    const avatar = await Jimp.read(avatarURL);
-    avatar.getBuffer(Jimp.MIME_PNG, async (err, buffer) => {
-     if (err) throw new Error(err);
-   
-     // Convert WebP to PNG using sharp
-     sharp(buffer)
-       .toFormat('png')
-       .toBuffer()
-       .then(async (convertedBuffer) => {
-         const avatarImage = await Jimp.read(convertedBuffer);
-         avatarImage.composite(overlay, 0, 0, {
-           mode: Jimp.BLEND_SOURCE_OVER,
-           opacityDest: 1,
-           opacitySource: 0.5,
-         });
-   
-         const finalBuffer = await avatarImage.getBufferAsync(Jimp.MIME_PNG);
-         const attachment = { files: [{ attachment: finalBuffer, name: 'jail.png' }] };
-   
-         const embed = new EmbedBuilder()
-           .setTitle(`${user.username} is in jail!`)
-           .setImage('attachment://jail.png');
-   
-         await interaction.reply({ embeds: [embed], files: [attachment] });
-       })
-  }),
-}
-}
+    avatarJimp.composite(overlay, 0, 0, {
+      mode: Jimp.BLEND_SOURCE_OVER,
+      opacityDest: 1,
+      opacitySource: 0.5,
+    });
+
+    // Convert the modified image to a buffer
+    const buffer = await avatarJimp.getBufferAsync(Jimp.MIME_PNG);
+
+    // Create an attachment from the buffer
+    const attachment = { files: [{ attachment: buffer, name: 'avatar.png' }] };
+
+    // Update the embed to use the attachment
+    const embed = new EmbedBuilder()
+      .setTitle(`${user.username}'s Avatar`)
+      .setImage('avatar.png'); // Set the image as an attachment
+
+    // Reply with the embed and the attachment
+    await interaction.reply({ embeds: [embed], files: [attachment] });
+  },
+};
